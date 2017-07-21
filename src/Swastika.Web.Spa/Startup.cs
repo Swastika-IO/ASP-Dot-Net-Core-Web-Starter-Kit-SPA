8db540474b72b96e6aaf28fcec6ef1c1654d6d54
Smilefounder <i.love.to.smile.around@gmail.com>
2017-07-22 01:54:43 +0700

    update

--- src/Swastika.Web.Spa/Startup.cs
+++ src/Swastika.Web.Spa/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Swastika_Web_Spa
 {
