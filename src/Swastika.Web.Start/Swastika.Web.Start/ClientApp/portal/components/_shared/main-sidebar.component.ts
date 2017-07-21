ac96c235ee4d4514d900be0de5c08968ecbd7c8b
Smilefounder <i.love.to.smile.around@gmail.com>
2017-07-22 00:58:42 +0700

    Update Base with dotnet core 2.0.0-preview3-25518-01

--- src/Swastika.Web.Start/Swastika.Web.Start/ClientApp/portal/components/_shared/main-sidebar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'main-sidebar',
-    templateUrl: './main-sidebar.component.html'
-})
-export class MainSidebarComponent {
-}
--- src/Swastika.Web.Start/Swastika.Web.Start/ClientApp/portal/components/pages/add-article.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'add-article',
-    templateUrl: './add-article.component.html'
-})
-export class AddArticleComponent {
-}
